2fb9bc8004b8e68f256adfb5741a64aaeb3742fa
Arttu Miinalainen <arttu.miinalainen01@gmail.com>
2018-02-14 16:39:28 +0200

    moi

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Helu!</div>
+  <div>Helou!</div>
 );
 
 class App extends Component {
